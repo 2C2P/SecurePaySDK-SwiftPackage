07e6c7c29615607d6a91390735f0c1425d1d0e24
Davidbilly Billy <davidbilly@2c2p.com>
2024-08-02 16:33:39 +0800

    - Update for SecurePay SDK v2.1.0.

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SecurePay",
-            url:"https://2c2p-cloudfront.s3.ap-southeast-1.amazonaws.com/devPortal/demoCode/Mobile/PGW_SDK/SecurePay/SECUREPAY_SDK_iOS_v2.0.0.zip",
-            checksum: "d7398703e227eed5ed405a8ef378fb8747962e87804c13c4e2e081be158af3a2"
+            url:"https://2c2p-cloudfront.s3.ap-southeast-1.amazonaws.com/devPortal/demoCode/Mobile/PGW_SDK/SecurePay/SECUREPAY_SDK_iOS_v2.1.0.zip",
+            checksum: "e4dc5a40a63c78df66820bc0c99802fce70f333f38471c838d9d2ab917886c7c"
         )
     ]
 )
